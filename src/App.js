c47dae37a8bb88666194e3022cd6edf76fd2d914
anton1110 <pavlovanton1110@gmail.com>
2022-07-14 11:38:24 +1000

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import PostCard from "./components/Card"
 
-function App() {
+const ButtonClick = () => {
+  const [posts, setPosts] = useState([])
+
+  const fetchData = () => {
+    fetch('https://frozen-river-19022.herokuapp.com/list')
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setPosts(data)
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={fetchData}>Fetch Posts</button>
+      {posts.map(post => (
+        <PostCard key={post.id} title={post.title} body={post.body} />
+      ))}
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default ButtonClick
---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../App.css';
+
+export function PostCard(props) {
+
+    
+  return (
+    <div className="card">
+      <h1>{props.title}</h1>
+      <div>
+        <p>
+          {props.body}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default PostCard;
